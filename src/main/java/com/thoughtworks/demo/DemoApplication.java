2818556da23b898f46d0a465540f1f08a318ac89
Rajesh Kumar <rajeshk@thoughtworks.com>
2018-09-05 12:20:45 +0530

    Swagger docs

--- src/main/java/com/thoughtworks/demo/DemoApplication.java
+++ src/main/java/com/thoughtworks/demo/DemoApplication.java
@@ -2,21 +2,13 @@ package com.thoughtworks.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class DemoApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(DemoApplication.class, args);
   }
-
-//  @Bean
-//  public CommandLineRunner getCommandLineRunner(ProductRepository repository) {
-//    return args -> {
-//      List<Product> products = new LinkedList<>();
-//      products.add(new Product("1", "Demo Prduct 1", "Demo Description 1"));
-//      products.add(new Product("2", "Demo Prduct 2", "Demo Description 2"));
-//      repository.insert(products);
-//    };
-//  }
 }
